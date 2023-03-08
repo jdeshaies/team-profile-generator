17849e3c3dd0a60ec0d6d4a39d8f62f647e9cbee
jd479800 <jd479800@gmail.com>
2023-03-07 20:07:00 -0500

    Adds employee contrusctor

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
+class Employee {
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+  getName() {
+    return this.name;
+  }
+  getId() {
+    return this.id;
+  }
+  getEmail() {
+    return this.email;
+  }
+  getRole() {
+    return 'Employee';
+  }
+}
+
 inquirer
   .prompt([
     {
